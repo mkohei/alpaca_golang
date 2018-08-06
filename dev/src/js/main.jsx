9248c07e48cc1e19d7f0dfd6bf057ad69ee233b3
mkohei <baberugaberiimeronnv5@gmail.com>
2018-08-06 14:15:18 +0900

    Client-comments

--- dev/src/js/main.jsx
+++ dev/src/js/main.jsx
@@ -1,30 +1,62 @@
 //ReactDOM.render( <h1>Hello, world!</h1>, document.getElementById('root') )
-const ItemList = props => {
+const LikeList = props => {
     return <ul className="item-list">
-    {props.likes.map(item => <ItemDetail item={item} />)}
+    {props.likes.map(item => <LikeDetail item={item} />)}
     </ul>;
 };
 
-const ItemDetail = props => {
+const LikeDetail = props => {
     const item = props.item;
     return <li>
         <div className="item-time">{item.Timestamp}</div>
     </li>;
 };
 
+const CommentList = props => {
+    return <ul className="item-list">
+    {props.comments.map(item => <CommentDetail item={item} />)}
+    </ul>;
+};
+
+const CommentDetail = props => {
+    const item = props.item;
+    return <li>
+        <div className="item-time">{item.Timestamp}</div>
+        <div className="item-time">{item.Content}</div>
+    </li>;
+};
+
+
 
 fetch('/api/likes').then(res => res.json()).then(data => {
     ReactDOM.render(
-        <ItemList likes={data.Likes} />, // これを
-        document.getElementById('root') // ここにレンダリングしろ
+        <LikeList likes={data.Likes} />, // これを
+        document.getElementById('likes-board') // ここにレンダリングしろ
+    );
+});
+
+fetch('api/comments').then(res => res.json()).then(data => {
+    ReactDOM.render(
+        <CommentList comments={data.Comments} />,
+        document.getElementById('comments-board')
     );
 });
 
 document.getElementById('like').onclick = function() {
-    //
     fetch('api/likes', {
         method : 'POST',
         body : '',
     }).then(res => res.text())
     .then(text => console.log(text));
+};
+
+document.getElementById('comment').onclick = function() {
+    const content = document.getElementById("content").value;
+    if (content.length > 0) {
+        fetch('api/comments', {
+            method : 'POST',
+            body : 'content=' + content,
+        }).then(res => res.text())
+        .then(text => console.log(text));
+    }
 };
